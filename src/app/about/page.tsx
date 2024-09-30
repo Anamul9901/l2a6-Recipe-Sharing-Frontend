cef26e495222feb566b38f281f715b6ddbe0173d
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-30 10:05:50 +0600

    Setup client

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+    return (
+        <div>
+            this is about
+        </div>
+    );
+};
+
+export default page;
---
+++ src/components/UI/navberDropdown.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { Avatar } from "@nextui-org/avatar";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
+import { useRouter } from "next/navigation";
+
+const NavberDropdown = () => {
+  const router = useRouter();
+
+  const handleLogOut = () => {
+ console.log('logout');
+  };
+
+  const handleNavigation = (pathname: string) => {
+    router.push(pathname);
+  };
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Avatar className="cursor-pointer" name="ana" />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem onClick={() => handleNavigation("/profile")}>
+          Profile
+        </DropdownItem>
+
+        <DropdownItem onClick={() => handleNavigation("/profile/settings")}>
+          Settings
+        </DropdownItem>
+        <DropdownItem onClick={() => handleNavigation("/profile/create-post")}>
+          Create Post
+        </DropdownItem>
+        <DropdownItem
+          onClick={() => handleLogOut()}
+          className="text-danger"
+          color="danger"
+        >
+          Logout
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export default NavberDropdown;
