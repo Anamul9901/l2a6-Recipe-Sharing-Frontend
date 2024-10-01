fe813c9116fa0a6f0a86ad86c329c0c8a70cda70
Anamul Haque <anamulhaque9901@gmail.com>
2024-10-02 03:02:08 +0600

    Add rating function

--- src/components/Card/RecipeCard.tsx
+++ src/components/Card/RecipeCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { selectCurrentUser } from "@/src/redux/features/auth/authSlice";
 import {
   useAddRatingOrUpvoteMutation,
@@ -12,9 +12,12 @@ import {
 import { useAppSelector } from "@/src/redux/hooks";
 import CommentModal from "../modals/CommentModal";
 import { useGetAllCommentQuery } from "@/src/redux/features/comment/commentApi";
+import { FaStar } from "react-icons/fa";
 
 const RecipeCard = ({ recipe }: { recipe: any }) => {
   const [recipeId, setRecipeId] = useState("");
+  const [userRating, setUserRating] = useState(0);
+  const [hoverRating, setHoverRating] = useState(0);
   const user = useAppSelector(selectCurrentUser);
   const loggedUserEmail = user?.user?.email;
   const { data: getAllRecipe } = useGetAllRecipeQuery(undefined);
@@ -145,73 +148,135 @@ const RecipeCard = ({ recipe }: { recipe: any }) => {
     }
   };
 
+  // Handle rating submission
+  const handleRating = async (ratingValue: number, id: string) => {
+    setUserRating(ratingValue);
+
+    //current post rating length
+    const filterRating = getAllRatingAndUpvote?.data?.filter(
+      (rating: any) => rating?.postId == id
+    );
+    const currentNumberOfUser = filterRating?.length + 1;
+
+    // Logic to save rating to the backend
+    const ratingData = {
+      postId: id,
+      userEmail: loggedUserEmail,
+      type: "rating",
+      rating: userRating,
+    };
+
+    const findRating = await getAllRatingAndUpvote?.data?.find(
+      (item: any) =>
+        item?.postId == id &&
+        item?.type == "rating" &&
+        item?.userEmail == loggedUserEmail
+    );
+    if (!findRating) {
+      const findRecipe = await getAllRecipe?.data?.find(
+        (recipe: any) => recipe?._id == id
+      );
+      const newRating = userRating;
+      const currentAverageRating = findRecipe?.rating;
+      const totalRating = currentAverageRating * currentNumberOfUser;
+      const newTotalRating = totalRating + newRating;
+      const newNumberOfUsers = currentNumberOfUser + 1;
+      const newAvarageRating = newTotalRating / newNumberOfUsers;
+      const roundedAvarageRating = parseFloat(newAvarageRating.toFixed(1));
+      const finalData = { id, data: { rating: roundedAvarageRating } };
+      const res1 = await updateRecipe(finalData).unwrap();
+
+      const res = await addUpvote(ratingData).unwrap();
+    }
+  };
+
   return (
-    <div className="max-w-md mx-auto bg-default-100 rounded-xl overflow-hidden shadow-lg mb-6 transform transition-all duration-500 hover:scale-105 hover:shadow-2xl">
+    <div className="max-w-md mx-auto bg-gradient-to-b from-gray-800 via-gray-900 to-black rounded-xl overflow-hidden shadow-2xl mb-6 transform transition-all duration-500 hover:scale-105 hover:shadow-neon">
       {/* Recipe Image */}
-      <img
-        className="w-full h-56 object-cover filter brightness-90 hover:brightness-110 transition-all duration-500"
-        src="https://i.ibb.co.com/pX5YXS6/tra-4.jpg"
-        alt={recipe?.title}
-      />
+      <div className="relative">
+        <img
+          className="w-full h-56 object-cover filter brightness-75 hover:brightness-100 transition-all duration-500"
+          src={
+            recipe?.image ||
+            "https://i.ibb.co.com/kBNtTmC/No-Image-Available.jpg"
+          }
+          alt={recipe?.title}
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50"></div>
+      </div>
 
       {/* Recipe Details */}
       <div className="p-6">
-        <h2 className="text-2xl font-bold text-default-900 mb-3 tracking-wider">
+        <h2 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-purple-500 mb-3 tracking-wide">
           {recipe?.title}
         </h2>
-        <p className="text-default-900 mb-4 leading-relaxed tracking-wide">
-          {recipe?.description}
-        </p>
 
+        {/* Rating and Premium Status */}
         <div className="flex items-center justify-between mb-4">
-          <span className="text-sm text-default-900 tracking-widest">
-            Rating: {recipe?.rating}
-          </span>
-        </div>
-
-        {/* Post and Premium Status */}
-        <div className="flex items-center justify-between mb-4">
-          <span className="text-sm text-default-900 tracking-wide">
-            Posted by: {recipe?.publishUser}
-          </span>
+          {/* Rating */}
+          <div>
+            <span className="text-sm text-teal-400 tracking-widest glow">
+              Rating: {recipe?.rating}
+            </span>
+            {/* Rating Section */}
+            <div className="flex items-center mb-4">
+              <span className="text-sm text-teal-400 tracking-widest mr-4">
+                Rate:
+              </span>
+              {Array(5)
+                .fill(0)
+                .map((_, index) => (
+                  <FaStar
+                    key={index}
+                    size={24}
+                    className={`cursor-pointer transition-all ${
+                      (hoverRating || userRating) > index
+                        ? "text-yellow-400"
+                        : "text-gray-500"
+                    }`}
+                    onClick={() => handleRating(index + 1, recipe?._id)}
+                    onMouseEnter={() => setUserRating(index + 1)}
+                    onMouseLeave={() => setHoverRating(0)}
+                  />
+                ))}
+            </div>
+          </div>
           <span
-            className={`text-sm font-bold tracking-wide ${
-              recipe?.isPremium ? "text-yellow-500 glow" : "text-gray-400"
+            className={`text-sm font-bold tracking-wider ${
+              recipe?.isPremium ? "text-yellow-400 glow-neon" : "text-gray-500"
             }`}
           >
             {recipe?.isPremium ? "Premium" : "Free"}
           </span>
         </div>
 
-        <div className="flex items-center justify-between">
-          {/* Upvote / Downvote Buttons */}
-          <div className="flex space-x-4">
-            <button
-              onClick={() => handleUpvote(recipe?._id)}
-              className={`px-4 py-2 rounded-lg transition-all duration-500 tracking-wider ${
-                isUpvoted
-                  ? "bg-gradient-to-r from-blue-500 to-purple-500 text-white"
-                  : "bg-gray-200 text-gray-800"
-              } hover:from-teal-400 hover:to-blue-600 focus:outline-none shadow-lg transform hover:scale-105`}
-            >
-              Up: {recipe?.upvote}
-            </button>
-            <button
-              onClick={() => handleDownVote(recipe?._id)}
-              className={`px-4 py-2 rounded-lg transition-all duration-500 tracking-wider ${
-                isDownvoted
-                  ? "bg-gradient-to-r from-blue-500 to-purple-500 text-white"
-                  : "bg-gray-200 text-gray-800"
-              } hover:from-teal-400 hover:to-blue-600 focus:outline-none shadow-lg transform hover:scale-105`}
-            >
-              Down: {recipe?.downvote}
-            </button>
-          </div>
+        {/* Upvote / Downvote Buttons */}
+        <div className="flex items-center justify-between space-x-4">
+          <button
+            onClick={() => handleUpvote(recipe?._id)}
+            className={`px-5 py-2 rounded-full transition-all duration-500 tracking-wider bg-gradient-to-r ${
+              isUpvoted
+                ? "from-teal-400 to-purple-500 text-white"
+                : "from-gray-700 to-gray-900 text-gray-400"
+            } hover:from-blue-500 hover:to-purple-600 focus:outline-none shadow-neon transform hover:scale-105`}
+          >
+            👍 {recipe?.upvote}
+          </button>
+          <button
+            onClick={() => handleDownVote(recipe?._id)}
+            className={`px-5 py-2 rounded-full transition-all duration-500 tracking-wider bg-gradient-to-r ${
+              isDownvoted
+                ? "from-teal-400 to-purple-500 text-white"
+                : "from-gray-700 to-gray-900 text-gray-400"
+            } hover:from-blue-500 hover:to-purple-600 focus:outline-none shadow-neon transform hover:scale-105`}
+          >
+            👎 {recipe?.downvote}
+          </button>
 
           {/* Comment Modal */}
           <div
             onClick={() => setRecipeId(recipe?._id)}
-            className="text-gray-600 hover:text-teal-500 cursor-pointer transition-all duration-500 transform hover:scale-110"
+            className="text-teal-400 hover:text-purple-500 cursor-pointer transition-all duration-500 transform hover:scale-110 glow-neon"
           >
             <CommentModal id={recipeId} comments={filterComment} />
           </div>
--- src/components/modals/CommentModal.tsx
+++ src/components/modals/CommentModal.tsx
@@ -14,9 +14,8 @@ import { verifyToken } from "../../utils/verifyToken";
 import { Avatar } from "@nextui-org/avatar";
 import Link from "next/link";
 import { useGetMyDataQuery } from "@/src/redux/features/user/userApi";
-//{ id }: { id: string }
+
 const CommentModal = ({ id, comments }: { id: string; comments: any }) => {
-  console.log("comments-", comments);
   const user = useAppSelector(selectCurrentUser);
 
   let verifyUser: any;
@@ -26,7 +25,6 @@ const CommentModal = ({ id, comments }: { id: string; comments: any }) => {
   const [addComment, { isLoading }] = useAddCommentMutation();
   const { data: currentUserData } = useGetMyDataQuery(undefined);
   const currentUser = currentUserData?.data[0];
-  console.log(currentUser);
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     const finalData = {
@@ -37,7 +35,6 @@ const CommentModal = ({ id, comments }: { id: string; comments: any }) => {
       commentUserName: currentUser?.name,
     };
     const res = await addComment(finalData).unwrap();
-    console.log("coment-", res);
   };
   return (
     <div className="relative  p-6">
@@ -50,9 +47,9 @@ const CommentModal = ({ id, comments }: { id: string; comments: any }) => {
     >
       <div className="space-y-6">
         <h1 className="text-3xl font-extrabold text-white mb-4">All Comments</h1>
-        <div className="space-y-6">
+        <div className="space-y-2">
           {comments?.map((comment: any) => (
-            <div className="border border-gray-700 rounded-xl p-4 shadow-lg bg-gray-800 bg-opacity-60 flex items-center gap-4 hover:bg-gray-700 transition-all duration-300 transform hover:scale-105">
+            <div className="border border-gray-700 rounded-xl p-2 shadow-lg bg-gray-800 bg-opacity-60 flex items-center gap-4 hover:bg-gray-700 transition-all duration-300 transform hover:scale-105">
               <Link href={`/profile/${comment?.commentUserId}`}>
                 <Avatar
                   src={comment?.commentUserImage}
@@ -62,11 +59,11 @@ const CommentModal = ({ id, comments }: { id: string; comments: any }) => {
               <div className="flex flex-col">
                 <Link
                   href={`/profile/${comment?.commentUserId}`}
-                  className="text-lg font-bold text-teal-400 hover:text-blue-400 transition-all duration-300"
+                  className="text-sm font-bold text-teal-400 hover:text-blue-400 transition-all duration-300"
                 >
                   {comment?.commentUserName}
                 </Link>
-                <p className="text-sm text-gray-300 mt-1">Comment: {comment?.comment}</p>
+                <p className="text-md text-gray-300 mt-1">Comment: {comment?.comment}</p>
               </div>
             </div>
           ))}
